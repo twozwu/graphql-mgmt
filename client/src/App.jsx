88312178c2d6b6d12109a372cd327a657a28cedb
Mix <honter_tribor@hotmail.com>
2022-08-17 02:45:53 +0800

    fixed client uri

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -29,7 +29,8 @@ const cache = new InMemoryCache({
 });
 
 const client = new ApolloClient({
-  uri: "http://localhost:5000/graphql",
+  // uri: "http://localhost:5000/graphql",
+  uri: "https://graphql-mgmt-course.herokuapp.com/graphql",
   cache,
 });
 
